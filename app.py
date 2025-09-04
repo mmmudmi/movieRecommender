6dcb0a742994d504bc4c62424854ea54983314d8
Mudmi <mmmudmi@gmail.com>
2025-09-04 15:45:03 -0500

    Update ui

--- app.py
+++ app.py
@@ -13,16 +13,14 @@ def fetch_poster(movie_id):
     backdrop_path = "https://image.tmdb.org/t/p/w500/" + data['backdrop_path']
     return poster_path, backdrop_path
 
-def recommend(movie):
-    # Get the index of the movie from our DataFrame
-    index = movies[movies['title'] == movie].index[0]
+def recommend(index):
     # Get similarity scores for this movie with all other movies
     similarity_scores = similarity[index]
     # Convert to list of tuples: (index, similarity_score)
     distances = sorted(list(enumerate(similarity_scores)), reverse=True, key=lambda x: x[1])
     recommendations = []
     
-    for i in distances[1:6]:
+    for i in distances[1:9]:
         movie_data = movies.iloc[i[0]]
         movie_id = movie_data.id
         poster, backdrop = fetch_poster(movie_id)
@@ -45,12 +43,37 @@ def home():
     movie_list = [(index, title) for index, title in enumerate(movies['title'].values)]
     return render_template('home.html', movies=movie_list)
 
-@app.route('/recommend', methods=['POST'])
-def get_recommendations():
-    selected_movie = movies.iloc[int(request.form['movie'])].title
-    recommendations = recommend(selected_movie)
+@app.route('/recommend/<int:movie_code>')
+def get_recommendations(movie_code):
+    recommendations = recommend(movie_code)
     return render_template('recommendations.html', recommendations=recommendations)
 
+@app.route('/search')
+def search():
+    query = request.args.get('query', '').lower()
+    if not query:
+        return render_template('home.html', movies=[(index, title) for index, title in enumerate(movies['title'].values)])
+    
+    # Filter movies based on the search query
+    search_results = []
+    for index, title in enumerate(movies['title'].values):
+        if query in title.lower():
+            movie_id = movies.iloc[index].id
+            poster, backdrop = fetch_poster(movie_id)
+            search_results.append({
+                'index': index,
+                'title': title,
+                'poster_url': poster,
+                'backdrop_url': backdrop,
+                'genres': movies.iloc[index].genres if 'genres' in movies.iloc[index] else ''
+            })
+    
+    return render_template('search.html', recommendations=search_results)
+
+@app.route('/random')
+def random_recommendation():
+    return render_template('random.html')
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
