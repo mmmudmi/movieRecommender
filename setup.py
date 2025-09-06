4714bbf88ae683f5c124c095ab4a0ab8e474ffca
Mudmi <mmmudmi@gmail.com>
2025-09-05 21:48:13 -0500

    update

--- setup.py
+++ setup.py
@@ -1,35 +1,18 @@
-from setuptools import setup
-
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-## edit below variables as per your requirements -
-
-REPO_NAME = "movieRecommender"
-AUTHOR_USER_NAME = "mmmudmi"
-LIST_OF_REQUIREMENTS = [
-    'flask>=2.3.3',
-    'requests>=2.31.0',
-    'numpy>=1.24.3',
-    'pandas>=2.0.3',
-    'scikit-learn>=1.3.0',
-    'gunicorn>=21.2.0',
-    'Frozen-Flask>=0.18'
-]
-
+from setuptools import setup, find_packages
 
 setup(
-    name=REPO_NAME,
+    name="movieRecommender",
     version="0.0.1",
-    author=AUTHOR_USER_NAME,
-    description="Movie Recommender System",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="mmmudmi@gmail.com",
-    packages=["movieRecommender"],
-    package_dir={"movieRecommender": "."},
+    packages=find_packages(),
     include_package_data=True,
-    python_requires=">=3.7",
-    install_requires=LIST_OF_REQUIREMENTS
+    install_requires=[
+        'flask>=2.3.3',
+        'requests>=2.31.0',
+        'numpy>=1.24.3',
+        'pandas>=2.0.3',
+        'scikit-learn>=1.3.0',
+        'gunicorn>=21.2.0',
+        'Frozen-Flask>=0.18'
+    ],
+    python_requires=">=3.8,<3.12"
 )
